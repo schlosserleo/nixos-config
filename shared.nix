42a7137874ed41b4c4df6da7b328fb2fb8705c8a
Leo Schlosser <leoschlosser@tutamail.com>
2025-08-21 01:51:58 +0200

    fix config

--- shared.nix
+++ shared.nix
@@ -15,6 +15,12 @@
 
   nix = {
     settings = {
+    substituters = [
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
       experimental-features = [
         "nix-command"
         "flakes"
