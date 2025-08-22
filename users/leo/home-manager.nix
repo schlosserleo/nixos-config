b3ac87f7c3c7e935af77eb39c6cc68b62a695eda
Leo Schlosser <leoschlosser@tutamail.com>
2025-08-22 13:38:27 +0200

    conf ghostty

--- users/leo/home-manager.nix
+++ users/leo/home-manager.nix
@@ -37,7 +37,6 @@ in
       tealdeer
       ripgrep
       chromium
-      ghostty
       nautilus
       gnome-tweaks
       fastfetch
@@ -49,6 +48,41 @@ in
     };
   };
   programs = {
+    ghostty = {
+      enable = true;
+      enableFishIntegration = true;
+      installVimSyntax = true;
+      settings = {
+        theme = "moonfly";
+      };
+      themes = {
+        moonfly = {
+          background = "#080808";
+          foreground = "#bdbdbd";
+          selection-background = "#b2ceee";
+          selection-foreground = "#080808";
+          cursor-color = "#8e8e8e";
+          palette = [
+            "0=#323437" 
+            "1=#ff5d5d"
+            "2=#8cc85f"
+            "3=#e3c78a"
+            "4=#80a0ff"
+            "5=#cf87e8"
+            "6=#79dac8"
+            "7=#c6c6c6"
+            "8=#949494"
+            "9=#ff5189"
+            "10=#36c692"
+            "11=#c6c684"
+            "12=#74b2ff"
+            "13=#ae81ff"
+            "14=#85dc85"
+            "15=#e4e4e4"
+          ];
+        };
+      };
+    };
     gpg = {
       enable = true;
       homedir = "/home/leo/.gnupg";
