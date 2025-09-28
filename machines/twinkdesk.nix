1a74b1d7c6fc200448b93dc85d7ff4712cc7fae2
Leo Schlosser <leoschlosser@tutamail.com>
2025-09-28 17:37:28 +0200

    update

--- machines/twinkdesk.nix
+++ machines/twinkdesk.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ config, ... }:
 {
   imports = [
     ./hardware/twinkdesk.nix
     ../shared.nix
   ];
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = false;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
 }
