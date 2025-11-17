b65fd803151e729840ed2c5d4c843cf0544ffcfc
Leo Schlosser <leoschlosser@tutamail.com>
2025-11-17 20:29:57 +0100

    extensions

--- modules/dconf.nix
+++ modules/dconf.nix
@@ -1,5 +1,7 @@
 { lib, ... }:
 
+with lib.hm.gvariant;
+
 {
   dconf.enable = true;
   dconf.settings = {
@@ -13,11 +15,52 @@
     "org/gnome/desktop/input-sources" = {
       show-all-sources = true;
       sources = [
-        (lib.gvariant.mkTuple [
+        (mkTuple [
           "xkb"
           "de+neo_qwertz"
         ])
       ];
     };
+    "org/gnome/desktop/wm/keybindings" = {
+      minimize = [ ];
+      move-to-workspace-1 = [ "<Shift><Super>1" ];
+      move-to-workspace-2 = [ "<Shift><Super>2" ];
+      move-to-workspace-3 = [ "<Shift><Super>3" ];
+      move-to-workspace-4 = [ "<Shift><Super>4" ];
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      toggle-fullscreen = [ "<Super>f" ];
+      toggle-maximized = [ "<Shift><Super>f" ];
+    };
+    "org/gnome/shell/keybindings" = {
+      show-screenshot-ui = [ "<Shift><Super>s" ];
+      switch-to-application-1 = [ ];
+      switch-to-application-2 = [ ];
+      switch-to-application-3 = [ ];
+      switch-to-application-4 = [ ];
+    };
+    "org/gnome/desktop/session" = {
+      idle-delay = mkUint32 0;
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+    };
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "flat";
+    };
+    "org/gnome/desktop/peripherals/keyboard" = {
+      delay = mkUint32 319;
+      repeat-interval = mkUint32 16;
+    };
+
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "dash-to-dock@micxgx.gmail.com"
+      ];
+    };
+
   };
 }
